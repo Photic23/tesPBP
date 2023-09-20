a55ed80068e4b266dc0700eb3ae9bba20249dcab
Photic23 <resellcourtesyx@gmail.com>
2023-09-20 14:53:26 +0700

    tespush

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.http import HttpResponseRedirect
 from main.forms import ProductForm
 from django.urls import reverse
@@ -5,13 +8,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 from main.models import Product
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(user=request.user)
+
     context = {
-        'name': 'Pak Bepe', # Nama kamu
-        'class': 'PBP A', # Kelas PBP kamu
-        'products': products
+        'name': request.user.username,
+        'class': 'PBP E', # Kelas PBP kamu
+        'products': products,
+        'last_login': request.COOKIES['last_login'],
     }
     return render(request, "main.html", context)
 
@@ -24,14 +35,13 @@ def show_json_by_id(request, id):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def create_product(request):
-    form = ProductForm(request.POST or None)
+ form = ProductForm(request.POST or None)
 
-    if form.is_valid() and request.method == "POST":
-        form.save()
-        return HttpResponseRedirect(reverse('main:show_main'))
-
-    context = {'form': form}
-    return render(request, "create_product.html", context)
+ if form.is_valid() and request.method == "POST":
+     product = form.save(commit=False)
+     product.user = request.user
+     product.save()
+     return HttpResponseRedirect(reverse('main:show_main'))
 
 def show_json(request):
     data = Product.objects.all()
@@ -41,4 +51,40 @@ def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
+def delete_element(request, id):
+    post = Product.objects.filter( pk=id) 
+    post.delete()
+    return HttpResponseRedirect('')
 
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
